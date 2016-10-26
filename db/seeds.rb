22b7eefe266c374806011bbe62d5208fb3ef92b6
Wojtek <wojtek@niom.pl>
2016-10-26 13:03:44 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(email: 'user@domain.com', password: 'password', password_confirmation: 'password')
-user = User.create(email: 'user2@domain.com', password: 'password', password_confirmation: 'password')
-group = Group.create(name: 'Office English', user: user)
+user2 = User.create(email: 'user2@domain.com', password: 'password', password_confirmation: 'password')
+group = Group.create(name: 'English Office Vocabulary', description: 'Words used in the office', user: user)
 Card.create(front: 'stapler', back: 'zszywacz', group: group)
 Card.create(front: 'appointment book', back: 'terminarz', group: group)
 Card.create(front: 'sellotape', back: 'taśma klejąca', group: group)
+Card.create(front: 'staple', back: 'zszywka', group: group)
+Card.create(front: 'stamp', back: 'pieczątka', group: group)
+Card.create(front: 'business card', back: 'wizytówka', group: group)
+group2 = Group.create(name: 'Another group without description', user: user)
+Card.create(front: 'hostile', back: 'wrogi', group: group2)
+Card.create(front: 'drawer', back: 'szuflada', group: group2)
 
